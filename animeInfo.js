8d39a78dbb717b2e4fc4383f8e1c01f59f53cbad
uunuu <unukubodiamar@gmail.com>
2023-11-05 23:54:27 +0800

    index dynamic bolson

---
+++ animeInfo.js
@@ -0,0 +1,279 @@
+const animeList = [
+  {
+    name: "Bleach",
+    releasedDate: 2003,
+    totalEpisode: 366,
+    totalDuration: 5400,
+    category: ["Action", "Shonen", "Fight"],
+    songType: "Rock",
+    malRank: 255,
+    animeImg: "https://i.ibb.co/XXWSzXY/bleach.png",
+    views: 11111,
+  },
+  {
+    name: "One Piece",
+    releasedDate: 1999,
+    totalEpisode: 1000,
+    totalDuration: 15000,
+    category: ["Adventure", "Shonen", "Pirate"],
+    songType: "Pop",
+    malRank: 4,
+    animeImg: "https://i.ibb.co/THCq70F/one-piece.jpg",
+    views: 8530,
+  },
+  {
+    name: "Naruto",
+    releasedDate: 2002,
+    totalEpisode: 220,
+    totalDuration: 3300,
+    category: ["Action", "Shonen", "Ninja"],
+    songType: "Hip-Hop",
+    malRank: 71,
+    animeImg: "https://i.ibb.co/7S8w68z/naruto.jpg",
+    views: 2354,
+  },
+  {
+    name: "Attack on Titan",
+    releasedDate: 2013,
+    totalEpisode: 75,
+    totalDuration: 1125,
+    category: ["Action", "Dark Fantasy", "Mystery"],
+    songType: "Orchestral",
+    malRank: 6,
+    animeImg: "https://i.ibb.co/QFSj2Hc/aot.jpg",
+    views: 1111,
+  },
+  {
+    name: "Death Note",
+    releasedDate: 2006,
+    totalEpisode: 37,
+    totalDuration: 555,
+    category: ["Mystery", "Psychological", "Supernatural"],
+    songType: "Classical",
+    malRank: 12,
+    animeImg: "https://i.ibb.co/nr187Qs/deathnote.jpg",
+    views: 899,
+  },
+  {
+    name: "Fullmetal Alchemist: Brotherhood",
+    releasedDate: 2009,
+    totalEpisode: 64,
+    totalDuration: 960,
+    category: ["Action", "Adventure", "Fantasy"],
+    songType: "Orchestral",
+    malRank: 1,
+    animeImg: "https://i.ibb.co/rMtmcMh/fma-bh.png",
+    views: 1684,
+  },
+  {
+    name: "Dragon Ball Z",
+    releasedDate: 1989,
+    totalEpisode: 291,
+    totalDuration: 4365,
+    category: ["Action", "Adventure", "Superpower"],
+    songType: "Rock",
+    malRank: 135,
+    animeImg: "https://i.ibb.co/7NFJvSZ/dbz.jpg",
+    views: 500,
+  },
+  {
+    name: "One Punch Man",
+    releasedDate: 2015,
+    totalEpisode: 24,
+    totalDuration: 360,
+    category: ["Action", "Comedy", "Superhero"],
+    songType: "J-Pop",
+    malRank: 17,
+    animeImg: "https://i.ibb.co/gRdGqkV/one-punch.jpg",
+    views: 632,
+  },
+  {
+    name: "Hunter x Hunter",
+    releasedDate: 2011,
+    totalEpisode: 148,
+    totalDuration: 2220,
+    category: ["Action", "Adventure", "Fantasy"],
+    songType: "Orchestral",
+    malRank: 7,
+    animeImg: "https://i.ibb.co/5jkdRnK/HxH.jpg",
+    views: 2300,
+  },
+  {
+    name: "My Hero Academia",
+    releasedDate: 2016,
+    totalEpisode: 113,
+    totalDuration: 1695,
+    category: ["Action", "Superpower", "School"],
+    songType: "J-Pop",
+    malRank: 5,
+    animeImg: "https://i.ibb.co/t8hc46V/my-Heroace.jpg",
+    views: 874,
+  },
+  {
+    name: "Cowboy Bebop",
+    releasedDate: 1998,
+    totalEpisode: 26,
+    totalDuration: 390,
+    category: ["Action", "Sci-Fi", "Space"],
+    songType: "Jazz",
+    malRank: 22,
+    animeImg: "https://i.ibb.co/vcKMV0f/cowboy.jpg",
+    views: 112,
+  },
+  {
+    name: "Demon Slayer",
+    releasedDate: 2019,
+    totalEpisode: 26,
+    totalDuration: 390,
+    category: ["Action", "Supernatural", "Demons"],
+    songType: "Orchestral",
+    malRank: 2,
+    animeImg: "https://i.ibb.co/4MRzMQ5/demon-slayer.jpg",
+    views: 447,
+  },
+  {
+    name: "Steins Gate",
+    releasedDate: 2011,
+    totalEpisode: 24,
+    totalDuration: 360,
+    category: ["Sci-Fi", "Thriller", "Time Travel"],
+    songType: "Electronic",
+    malRank: 3,
+    animeImg: "https://i.ibb.co/sQHM0PD/steins-gate.jpg",
+    views: 789,
+  },
+  {
+    name: "Neon Genesis Evangelion",
+    releasedDate: 1995,
+    totalEpisode: 26,
+    totalDuration: 390,
+    category: ["Mecha", "Psychological", "Sci-Fi"],
+    songType: "Classical",
+    malRank: 25,
+    animeImg: "https://i.ibb.co/v11QfzJ/neon-genises.jpg",
+    views: 456,
+  },
+  {
+    name: "Fairy Tail",
+    releasedDate: 2009,
+    totalEpisode: 175,
+    totalDuration: 2625,
+    category: ["Adventure", "Fantasy", "Magic"],
+    songType: "Pop",
+    malRank: 42,
+    animeImg: "https://i.ibb.co/dGSKB6m/dairy-tail.jpg",
+    views: 235,
+  },
+  {
+    name: "Sword Art Online",
+    releasedDate: 2012,
+    totalEpisode: 97,
+    totalDuration: 1455,
+    category: ["Action", "Adventure", "Game"],
+    songType: "J-Pop",
+    malRank: 32,
+    animeImg: "https://i.ibb.co/Pr9yjmL/sword-art-online.jpg",
+    views: 112,
+  },
+];
+
+class Anime {
+  constructor(animeListItem) {
+    this.name = animeListItem.name;
+    this.releasedDate = animeListItem.releasedDate;
+    this.totalEpisode = animeListItem.totalEpisode;
+    this.animeImg = animeListItem.animeImg;
+    this.malRank = animeListItem.malRank;
+    this.category = animeListItem.category;
+  }
+
+  getCatogeries = function() {
+    let categoryList = "";
+    for(let i = 0 ; i < this.category.length ; i++) {
+        if(i !== this.category.length -1 ) {
+            categoryList += `${this.category[i]}, `;
+        } else {
+            categoryList += `${this.category[i]}`;
+        }
+
+    }
+    return categoryList;
+  }
+
+  render = function () {
+    return `
+        <li>
+        <div class="anime-card">
+          <div class="anime-card-inner anime-card-front">
+            <img src="${this.animeImg}" alt="Recommended anime" />
+          </div>
+          <div class="anime-card-inner anime-card-back">
+            <div class="anime-card-back-content">
+              <p><strong>Нэр: </strong>${this.name}</p>
+              <p><strong>Гарсан он: </strong>${this.releasedDate}</p>
+              <p><strong>Ангийн тоо: </strong>${this.totalEpisode}</p>
+              <p><strong>Төрөл: <br></strong>${this.getCatogeries()}</p>
+            </div>
+            <button class="anime-card-button">Дэлгэрэнгүй</button>
+          </div>
+        </div>
+      </li>
+        `;
+  };
+}
+
+class App {
+  constructor(targetId) {
+    this.target = targetId;
+  }
+
+  init = function() {
+    const trgt = document.getElementById(this.target);
+    let count = 0;
+    for (const anime of animeList) {
+      if (count === 6) {
+        break;
+      }
+      const renderedAnime = new Anime(anime);
+      trgt.insertAdjacentHTML("beforeend", renderedAnime.render());
+      count++;
+    }
+  }
+
+  initAddedRecently = function () {
+    const initAddedRecentlyAnimes = this.sortByDate();
+    this.init();
+  };
+
+  initMostViews = function () {
+    const initAddedRecentlyAnimes = this.sortByViews();
+    this.init();
+  };
+
+  initMalRank = function () {
+    const initAddedRecentlyAnimes = this.sortByMalRank();
+    this.init();
+  };
+
+  sortByDate = function () {
+    const sortedArray = animeList.sort((anime1, anime2) => {
+      return anime2.releasedDate - anime1.releasedDate;
+    });
+    return sortedArray;
+  };
+
+  sortByViews = function () {
+    const sortedArray = animeList.sort((anime1, anime2) => {
+      return anime2.views - anime1.views;
+    });
+    return sortedArray;
+  };
+
+  sortByMalRank = function () {
+    const sortedArray = animeList.sort((anime1, anime2) => {
+      return anime1.malRank - anime2.malRank;
+    });
+    return sortedArray;
+  };
+
+}
