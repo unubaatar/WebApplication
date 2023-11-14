075ac050bb2d118348c9ef51546536c7219a62e2
uunuu <unukubodiamar@gmail.com>
2023-11-15 03:41:14 +0800

    shopping heseg bolson

--- animeInfo.js
+++ animeInfo.js
@@ -1,181 +1,264 @@
-const animeList = [
+animeList = [
   {
-    name: "Bleach",
-    releasedDate: 2003,
-    totalEpisode: 366,
-    totalDuration: 5400,
-    category: ["Action", "Shonen", "Fight"],
-    songType: "Rock",
-    malRank: 255,
-    animeImg: "https://i.ibb.co/XXWSzXY/bleach.png",
-    views: 11111,
+      "name": "Bleach",
+      "releasedDate": 2003,
+      "totalEpisode": 366,
+      "totalDuration": 5400,
+      "category": [
+          "Action",
+          "Shonen",
+          "Fight"
+      ],
+      "songType": "Rock",
+      "malRank": 255,
+      "animeImg": "https://i.ibb.co/XXWSzXY/bleach.png",
+      "views": 11111
   },
   {
-    name: "One Piece",
-    releasedDate: 1999,
-    totalEpisode: 1000,
-    totalDuration: 15000,
-    category: ["Adventure", "Shonen", "Pirate"],
-    songType: "Pop",
-    malRank: 4,
-    animeImg: "https://i.ibb.co/THCq70F/one-piece.jpg",
-    views: 8530,
+      "name": "One Piece",
+      "releasedDate": 1999,
+      "totalEpisode": 1000,
+      "totalDuration": 15000,
+      "category": [
+          "Adventure",
+          "Shonen",
+          "Pirate"
+      ],
+      "songType": "Pop",
+      "malRank": 4,
+      "animeImg": "https://i.ibb.co/THCq70F/one-piece.jpg",
+      "views": 8530
   },
   {
-    name: "Naruto",
-    releasedDate: 2002,
-    totalEpisode: 220,
-    totalDuration: 3300,
-    category: ["Action", "Shonen", "Ninja"],
-    songType: "Hip-Hop",
-    malRank: 71,
-    animeImg: "https://i.ibb.co/7S8w68z/naruto.jpg",
-    views: 2354,
+      "name": "Naruto",
+      "releasedDate": 2002,
+      "totalEpisode": 220,
+      "totalDuration": 3300,
+      "category": [
+          "Action",
+          "Shonen",
+          "Ninja"
+      ],
+      "songType": "Hip-Hop",
+      "malRank": 71,
+      "animeImg": "https://i.ibb.co/7S8w68z/naruto.jpg",
+      "views": 2354
   },
   {
-    name: "Attack on Titan",
-    releasedDate: 2013,
-    totalEpisode: 75,
-    totalDuration: 1125,
-    category: ["Action", "Dark Fantasy", "Mystery"],
-    songType: "Orchestral",
-    malRank: 6,
-    animeImg: "https://i.ibb.co/QFSj2Hc/aot.jpg",
-    views: 1111,
+      "name": "Attack on Titan",
+      "releasedDate": 2013,
+      "totalEpisode": 75,
+      "totalDuration": 1125,
+      "category": [
+          "Action",
+          "Dark Fantasy",
+          "Mystery"
+      ],
+      "songType": "Orchestral",
+      "malRank": 6,
+      "animeImg": "https://i.ibb.co/QFSj2Hc/aot.jpg",
+      "views": 1111
   },
   {
-    name: "Death Note",
-    releasedDate: 2006,
-    totalEpisode: 37,
-    totalDuration: 555,
-    category: ["Mystery", "Psychological", "Supernatural"],
-    songType: "Classical",
-    malRank: 12,
-    animeImg: "https://i.ibb.co/nr187Qs/deathnote.jpg",
-    views: 899,
+      "name": "Death Note",
+      "releasedDate": 2006,
+      "totalEpisode": 37,
+      "totalDuration": 555,
+      "category": [
+          "Mystery",
+          "Psychological",
+          "Supernatural"
+      ],
+      "songType": "Classical",
+      "malRank": 12,
+      "animeImg": "https://i.ibb.co/nr187Qs/deathnote.jpg",
+      "views": 899
   },
   {
-    name: "Fullmetal Alchemist: Brotherhood",
-    releasedDate: 2009,
-    totalEpisode: 64,
-    totalDuration: 960,
-    category: ["Action", "Adventure", "Fantasy"],
-    songType: "Orchestral",
-    malRank: 1,
-    animeImg: "https://i.ibb.co/rMtmcMh/fma-bh.png",
-    views: 1684,
+      "name": "Fullmetal Alchemist: Brotherhood",
+      "releasedDate": 2009,
+      "totalEpisode": 64,
+      "totalDuration": 960,
+      "category": [
+          "Action",
+          "Adventure",
+          "Fantasy"
+      ],
+      "songType": "Orchestral",
+      "malRank": 1,
+      "animeImg": "https://i.ibb.co/rMtmcMh/fma-bh.png",
+      "views": 1684
   },
   {
-    name: "Dragon Ball Z",
-    releasedDate: 1989,
-    totalEpisode: 291,
-    totalDuration: 4365,
-    category: ["Action", "Adventure", "Superpower"],
-    songType: "Rock",
-    malRank: 135,
-    animeImg: "https://i.ibb.co/7NFJvSZ/dbz.jpg",
-    views: 500,
+      "name": "Dragon Ball Z",
+      "releasedDate": 1989,
+      "totalEpisode": 291,
+      "totalDuration": 4365,
+      "category": [
+          "Action",
+          "Adventure",
+          "Superpower"
+      ],
+      "songType": "Rock",
+      "malRank": 135,
+      "animeImg": "https://i.ibb.co/7NFJvSZ/dbz.jpg",
+      "views": 500
   },
   {
-    name: "One Punch Man",
-    releasedDate: 2015,
-    totalEpisode: 24,
-    totalDuration: 360,
-    category: ["Action", "Comedy", "Superhero"],
-    songType: "J-Pop",
-    malRank: 17,
-    animeImg: "https://i.ibb.co/gRdGqkV/one-punch.jpg",
-    views: 632,
+      "name": "One Punch Man",
+      "releasedDate": 2015,
+      "totalEpisode": 24,
+      "totalDuration": 360,
+      "category": [
+          "Action",
+          "Comedy",
+          "Superhero"
+      ],
+      "songType": "J-Pop",
+      "malRank": 17,
+      "animeImg": "https://i.ibb.co/gRdGqkV/one-punch.jpg",
+      "views": 632
   },
   {
-    name: "Hunter x Hunter",
-    releasedDate: 2011,
-    totalEpisode: 148,
-    totalDuration: 2220,
-    category: ["Action", "Adventure", "Fantasy"],
-    songType: "Orchestral",
-    malRank: 7,
-    animeImg: "https://i.ibb.co/5jkdRnK/HxH.jpg",
-    views: 2300,
+      "name": "Hunter x Hunter",
+      "releasedDate": 2011,
+      "totalEpisode": 148,
+      "totalDuration": 2220,
+      "category": [
+          "Action",
+          "Adventure",
+          "Fantasy"
+      ],
+      "songType": "Orchestral",
+      "malRank": 7,
+      "animeImg": "https://i.ibb.co/5jkdRnK/HxH.jpg",
+      "views": 2300
   },
   {
-    name: "My Hero Academia",
-    releasedDate: 2016,
-    totalEpisode: 113,
-    totalDuration: 1695,
-    category: ["Action", "Superpower", "School"],
-    songType: "J-Pop",
-    malRank: 5,
-    animeImg: "https://i.ibb.co/t8hc46V/my-Heroace.jpg",
-    views: 874,
+      "name": "My Hero Academia",
+      "releasedDate": 2016,
+      "totalEpisode": 113,
+      "totalDuration": 1695,
+      "category": [
+          "Action",
+          "Superpower",
+          "School"
+      ],
+      "songType": "J-Pop",
+      "malRank": 5,
+      "animeImg": "https://i.ibb.co/t8hc46V/my-Heroace.jpg",
+      "views": 874
   },
   {
-    name: "Cowboy Bebop",
-    releasedDate: 1998,
-    totalEpisode: 26,
-    totalDuration: 390,
-    category: ["Action", "Sci-Fi", "Space"],
-    songType: "Jazz",
-    malRank: 22,
-    animeImg: "https://i.ibb.co/vcKMV0f/cowboy.jpg",
-    views: 112,
+      "name": "Cowboy Bebop",
+      "releasedDate": 1998,
+      "totalEpisode": 26,
+      "totalDuration": 390,
+      "category": [
+          "Action",
+          "Sci-Fi",
+          "Space"
+      ],
+      "songType": "Jazz",
+      "malRank": 22,
+      "animeImg": "https://i.ibb.co/vcKMV0f/cowboy.jpg",
+      "views": 112
   },
   {
-    name: "Demon Slayer",
-    releasedDate: 2019,
-    totalEpisode: 26,
-    totalDuration: 390,
-    category: ["Action", "Supernatural", "Demons"],
-    songType: "Orchestral",
-    malRank: 2,
-    animeImg: "https://i.ibb.co/4MRzMQ5/demon-slayer.jpg",
-    views: 447,
+      "name": "Demon Slayer",
+      "releasedDate": 2019,
+      "totalEpisode": 26,
+      "totalDuration": 390,
+      "category": [
+          "Action",
+          "Supernatural",
+          "Demons"
+      ],
+      "songType": "Orchestral",
+      "malRank": 2,
+      "animeImg": "https://i.ibb.co/4MRzMQ5/demon-slayer.jpg",
+      "views": 447
   },
   {
-    name: "Steins Gate",
-    releasedDate: 2011,
-    totalEpisode: 24,
-    totalDuration: 360,
-    category: ["Sci-Fi", "Thriller", "Time Travel"],
-    songType: "Electronic",
-    malRank: 3,
-    animeImg: "https://i.ibb.co/sQHM0PD/steins-gate.jpg",
-    views: 789,
+      "name": "Steins Gate",
+      "releasedDate": 2011,
+      "totalEpisode": 24,
+      "totalDuration": 360,
+      "category": [
+          "Sci-Fi",
+          "Thriller",
+          "Time Travel"
+      ],
+      "songType": "Electronic",
+      "malRank": 3,
+      "animeImg": "https://i.ibb.co/sQHM0PD/steins-gate.jpg",
+      "views": 789
   },
   {
-    name: "Neon Genesis Evangelion",
-    releasedDate: 1995,
-    totalEpisode: 26,
-    totalDuration: 390,
-    category: ["Mecha", "Psychological", "Sci-Fi"],
-    songType: "Classical",
-    malRank: 25,
-    animeImg: "https://i.ibb.co/v11QfzJ/neon-genises.jpg",
-    views: 456,
+      "name": "Neon Genesis Evangelion",
+      "releasedDate": 1995,
+      "totalEpisode": 26,
+      "totalDuration": 390,
+      "category": [
+          "Mecha",
+          "Psychological",
+          "Sci-Fi"
+      ],
+      "songType": "Classical",
+      "malRank": 25,
+      "animeImg": "https://i.ibb.co/v11QfzJ/neon-genises.jpg",
+      "views": 456
   },
   {
-    name: "Fairy Tail",
-    releasedDate: 2009,
-    totalEpisode: 175,
-    totalDuration: 2625,
-    category: ["Adventure", "Fantasy", "Magic"],
-    songType: "Pop",
-    malRank: 42,
-    animeImg: "https://i.ibb.co/dGSKB6m/dairy-tail.jpg",
-    views: 235,
+      "name": "Fairy Tail",
+      "releasedDate": 2009,
+      "totalEpisode": 175,
+      "totalDuration": 2625,
+      "category": [
+          "Adventure",
+          "Fantasy",
+          "Magic"
+      ],
+      "songType": "Pop",
+      "malRank": 42,
+      "animeImg": "https://i.ibb.co/dGSKB6m/dairy-tail",
+      "views": 235
   },
   {
-    name: "Sword Art Online",
-    releasedDate: 2012,
-    totalEpisode: 97,
-    totalDuration: 1455,
-    category: ["Action", "Adventure", "Game"],
-    songType: "J-Pop",
-    malRank: 32,
-    animeImg: "https://i.ibb.co/Pr9yjmL/sword-art-online.jpg",
-    views: 112,
+      "name": "Fairy Tail",
+      "releasedDate": 2009,
+      "totalEpisode": 175,
+      "totalDuration": 2625,
+      "category": [
+          "Adventure",
+          "Fantasy",
+          "Magic"
+      ],
+      "songType": "Pop",
+      "malRank": 42,
+      "animeImg": "https://i.ibb.co/dGSKB6m/dairy-tail.jpg",
+      "views": 235
   },
-];
+  {
+      "name": "Sword Art Online",
+      "releasedDate": 2012,
+      "totalEpisode": 97,
+      "totalDuration": [
+          "Action",
+          "Adventure",
+          "Game"
+      ],
+      "category": [
+          "Adventure",
+          "Fantasy",
+          "Magic"
+      ],
+      "songType": "J-Pop",
+      "malRank": 42,
+      "animeImg": "https://i.ibb.co/Pr9yjmL/sword-art-online.jpg",
+      "views": 112
+  }
+]
 
 class Anime {
   constructor(animeListItem) {
@@ -199,8 +282,6 @@ class Anime {
     }
     return categoryList;
   }
-
-
 
   render = function () {
     return `
@@ -279,19 +360,6 @@ class App {
     return sortedArray;
   };
 }
-
-// ene zuer try hiilt shuu api s function oo tatahjiisii
-getData = async function() {
-  try {
-    const response = await fetch("https://api.jsonbin.io/v3/b/654f4bb354105e766fce7c86");
-    let data = await response.json();
-    console.log(data.record)
-    const animesList = data.record;
-  } catch {
-    console.log("error");
-  }
-}
-
 
 // enuuger anime iin delgerengu page iihe dataga url aar damjulad avn deer render dree onclick der ni zaagd ugcin
 moveToAnimeDetailPage = function(name) {
