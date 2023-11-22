a5e9963e9c1ef8812a64fc84eb8eafb9cf001514
amirda <121286048+amirdaGankhuyag@users.noreply.github.com>
2023-11-22 22:55:43 +0800

    +

--- search.js
+++ search.js
@@ -1,5 +1,6 @@
 const apiUrl = "https://api.jsonbin.io/v3/b/655d6c2b0574da7622ca003f" 
  async function doSearch(search){
-    const result = await fetch(apiUrl).then(response => response.json()).then(data=> {
-        let animes = data.record;})
+    const result = await fetch(apiUrl + search).then(response => response.json()).then(data=> {
+        let animes = data.record;});
+        console.log(animes);
  }
