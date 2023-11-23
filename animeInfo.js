382207848a7b423a2235d886719dacb15205710e
uunuu <unukubodiamar@gmail.com>
2023-11-23 23:04:31 +0800

    air aldaa gartsn zasah shg bollo

--- animeInfo.js
+++ animeInfo.js
@@ -1,6 +1,12 @@
+// uunuu
 // const apiUrl = "https://api.jsonbin.io/v3/b/654f4bb354105e766fce7c86"
+
+// amirda
 //const apiUrl = "https://api.jsonbin.io/v3/b/655d6c2b0574da7622ca003f" 
+
+// haliunaa
 const apiUrl = "https://api.jsonbin.io/v3/b/655d82ba54105e766fd367c6"
+
 animeList = [
   {
     "name": "Bleach",
