4bd2c75c819cab3d4b129535850c7b3cc293f09e
uunuu <unukubodiamar@gmail.com>
2023-11-23 22:02:22 +0800

    filter url aar bolson

--- filter.js
+++ filter.js
@@ -1,5 +1,101 @@
 const apiUrl = "https://api.jsonbin.io/v3/b/654f4bb354105e766fce7c86"
 
+class Anime {
+  constructor(animeListItem) {
+    this.name = animeListItem.name;
+    this.releasedDate = animeListItem.releasedDate;
+    this.totalEpisode = animeListItem.totalEpisode;
+    this.animeImg = animeListItem.animeImg;
+    this.malRank = animeListItem.malRank;
+    this.category = animeListItem.category;
+  }
+
+  getCatogeries = function () {
+    let categoryList = "";
+    for (let i = 0; i < this.category.length; i++) {
+      if (i !== this.category.length - 1) {
+        categoryList += `${this.category[i]}, `;
+      } else {
+        categoryList += `${this.category[i]}`;
+      }
+    }
+    return categoryList;
+  };
+
+  render = function () {
+    return `
+          <li>
+          <div class="anime-card">
+            <div class="anime-card-inner anime-card-front">
+              <img src="${this.animeImg}" alt="Recommended anime" />
+            </div>
+            <div class="anime-card-inner anime-card-back">
+              <div class="anime-card-back-content">
+                <p><strong>Нэр: </strong>${this.name}</p>
+                <p><strong>Гарсан он: </strong>${this.releasedDate}</p>
+                <p><strong>Ангийн тоо: </strong>${this.totalEpisode}</p>
+                <p><strong>Төрөл: <br></strong>${this.getCatogeries()}</p>
+              </div>
+              <button class="anime-card-button" onclick= "moveToAnimeDetailPage('${this.name}')" >Дэлгэрэнгүй</button>
+            </div>
+          </div>
+        </li>
+          `;
+  };
+}
+
+
+
+let listCatogeries =  document.getElementById("categoryList").getElementsByTagName('li');
+for(let list of listCatogeries) {
+  list.addEventListener("click" , (event) => {
+    let categoryType = event.target.textContent
+    const nowUrl = new URL(window.location.href);
+    let newUrl = nowUrl.origin; 
+    newUrl += `/filter.html?category=${categoryType}`;
+    window.location.href = newUrl;
+  })
+}
+
+document.getElementById("animeSelection").addEventListener("change" , (event) => {
+  const categoryType = event.target.value;
+  const nowUrl = new URL(window.location.href);
+  let newUrl = nowUrl.origin; 
+  newUrl += `/filter.html?category=${categoryType}`;
+  window.location.href = newUrl;
+})
+
+
+document.addEventListener("DOMContentLoaded" , async() => {
+    let response = await fetch(apiUrl);
+    let data = await response.json();
+    let listOfAnime = await data.record;
+    let urlParams = new URLSearchParams(window.location.search);
+    let categoryType = urlParams.get("category");
+    console.log(categoryType);
+    document.getElementById("filteredType").value = categoryType;
+    let target = document.getElementById("filteredAnimeList");
+    let selectedAnimes = listOfAnime.filter((anime) => anime.category.includes(categoryType));
+    selectedAnimes.map((anime) => {
+      const renderedAnime = new Anime(anime);
+      target.insertAdjacentHTML("beforeend" , renderedAnime.render());
+    })
+})
+
+
+// enuuger anime iin delgerengu page iihe dataga url aar damjulad avn deer render dree onclick der ni zaagd ugcin
+moveToAnimeDetailPage = function(name) {
+  let nowUrl = new URL(window.location.href);
+  let newUrl = nowUrl.origin;
+  console.log(newUrl);
+  newUrl += `/animeDetails.html?name=${name}`
+  window.location.href = newUrl;
+}
+
+
+
+
+
 // const animeList = [
 //   {
 //     name: "Bleach",
@@ -179,106 +275,49 @@ const apiUrl = "https://api.jsonbin.io/v3/b/654f4bb354105e766fce7c86"
 //   },
 // ];
 
-class Anime {
-  constructor(animeListItem) {
-    this.name = animeListItem.name;
-    this.releasedDate = animeListItem.releasedDate;
-    this.totalEpisode = animeListItem.totalEpisode;
-    this.animeImg = animeListItem.animeImg;
-    this.malRank = animeListItem.malRank;
-    this.category = animeListItem.category;
-  }
+// let listCatogeries =  document.getElementById("categoryList").getElementsByTagName('li');
+// for(let category of listCatogeries) {
+//     category.addEventListener("click", function (event) {
+//         const clickedElement = event.target;
+//         document.getElementById("filteredType").value = clickedElement.textContent;
+//         let target = document.getElementById("filteredAnimeList");
+//         while(target.hasChildNodes()) {
+//           target.removeChild(target.firstChild)
+//         }
+//         fetch(apiUrl).then(response => response.json()).then(data => {
+//           let animes = data.record;
+//           for (const anime of animes) {
+//             for (let i = 0; i < anime.category.length; i++) {
+//               if (
+//                 anime.category[i] === document.getElementById("filteredType").value
+//               ) {
+//                 const renderedAnime = new Anime(anime);
+//                 target.insertAdjacentHTML("beforeend", renderedAnime.render());
+//                 break;  
+//               }
+//             }
+//           }
+//         })
+//       });
+// }
 
-  getCatogeries = function () {
-    let categoryList = "";
-    for (let i = 0; i < this.category.length; i++) {
-      if (i !== this.category.length - 1) {
-        categoryList += `${this.category[i]}, `;
-      } else {
-        categoryList += `${this.category[i]}`;
-      }
-    }
-    return categoryList;
-  };
-
-  render = function () {
-    return `
-          <li>
-          <div class="anime-card">
-            <div class="anime-card-inner anime-card-front">
-              <img src="${this.animeImg}" alt="Recommended anime" />
-            </div>
-            <div class="anime-card-inner anime-card-back">
-              <div class="anime-card-back-content">
-                <p><strong>Нэр: </strong>${this.name}</p>
-                <p><strong>Гарсан он: </strong>${this.releasedDate}</p>
-                <p><strong>Ангийн тоо: </strong>${this.totalEpisode}</p>
-                <p><strong>Төрөл: <br></strong>${this.getCatogeries()}</p>
-              </div>
-              <button class="anime-card-button" onclick= "moveToAnimeDetailPage('${this.name}')" >Дэлгэрэнгүй</button>
-            </div>
-          </div>
-        </li>
-          `;
-  };
-}
-
-let listCatogeries =  document.getElementById("categoryList").getElementsByTagName('li');
-for(let category of listCatogeries) {
-    category.addEventListener("click", function (event) {
-        const clickedElement = event.target;
-        document.getElementById("filteredType").value = clickedElement.textContent;
-        let target = document.getElementById("filteredAnimeList");
-        while(target.hasChildNodes()) {
-          target.removeChild(target.firstChild)
-        }
-        fetch(apiUrl).then(response => response.json()).then(data => {
-          let animes = data.record;
-          for (const anime of animes) {
-            for (let i = 0; i < anime.category.length; i++) {
-              if (
-                anime.category[i] === document.getElementById("filteredType").value
-              ) {
-                const renderedAnime = new Anime(anime);
-                target.insertAdjacentHTML("beforeend", renderedAnime.render());
-                break;  
-              }
-            }
-          }
-        })
-      });
-}
-
-document.getElementById("animeSelection").addEventListener("change" , (event) => {
-  const selectedElement = event.target;
-  document.getElementById("filteredType").value = selectedElement.value;
-  let target = document.getElementById("filteredAnimeList");
-  while(target.hasChildNodes()) {
-    target.removeChild(target.firstChild);
-  }
-  fetch(apiUrl).then(response => response.json()).then(data => {
-    let animes = data.record;
-    for(const anime of animes) {
-      for(let i = 0 ; i < anime.category.length ; i++) {
-        if(anime.category[i] === selectedElement.value) {
-          const renderedAnime = new Anime(anime);
-          target.insertAdjacentHTML("beforeend" , renderedAnime.render());
-          break;
-        }
-      }
-    }
-  })
-})
-
-// enuuger anime iin delgerengu page iihe dataga url aar damjulad avn deer render dree onclick der ni zaagd ugcin
-moveToAnimeDetailPage = function(name) {
-  console.log("hello");
-  let currentURL = window.location.href;
-  let newUrl = currentURL.replace("filter.html" , "animeDetails.html");
-  newUrl += `?name=${name}`
-  window.location.href = newUrl;
-}
-
-
-
-
+// document.getElementById("animeSelection").addEventListener("change" , (event) => {
+//   const selectedElement = event.target;
+//   document.getElementById("filteredType").value = selectedElement.value;
+//   let target = document.getElementById("filteredAnimeList");
+//   while(target.hasChildNodes()) {
+//     target.removeChild(target.firstChild);
+//   }
+//   fetch(apiUrl).then(response => response.json()).then(data => {
+//     let animes = data.record;
+//     for(const anime of animes) {
+//       for(let i = 0 ; i < anime.category.length ; i++) {
+//         if(anime.category[i] === selectedElement.value) {
+//           const renderedAnime = new Anime(anime);
+//           target.insertAdjacentHTML("beforeend" , renderedAnime.render());
+//           break;
+//         }
+//       }
+//     }
+//   })
+// })
